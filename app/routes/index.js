3de68bec28642bfe412aa6049f772fc937ffe75d
garryk3 <egoruum@gmail.com>
2017-08-25 00:40:30 +0300

    db

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,6 @@
 module.exports = function (app, db) {
     app.post('/test', (req, res) => {
         res.send('Hello')
+        console.log(req.body)
     });
 }
--- server.js
+++ server.js
@@ -2,9 +2,19 @@ const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
 const app            = express();
+const db             = require('./config/db');
 
 const port = 8000;
-require('./app/routes')(app, {});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+MongoClient.connect(db.url, (err, database) => {
+    if (err) return console.log(err)
+    require('./app/routes')(app, database);
+    app.listen(port, () => {
+        console.log('We are live on ' + port);
+    });
+})
 app.listen(port, () => {
     console.log('We are live on ' + port);
 });
